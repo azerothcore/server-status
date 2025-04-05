1b3ee904c1b119e54aaff3683816be8f2935ab94
Helias <stefanoborzi32@gmail.com>
2025-04-05 18:49:35 +0200

    chore: remove app.module.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { enableProdMode } from '@angular/core';
 
+import { provideHttpClient } from '@angular/common/http';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { provideHttpClient } from '@angular/common/http';
 
 if (environment.production) {
   enableProdMode();
