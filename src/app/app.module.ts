7ea5192fb9341a8f977cf767a41646915500bed7
Helias <stefanoborzi32@gmail.com>
2025-04-05 12:38:16 +0200

    refactor: use signals, standalone component, add enum, handle error response

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
-@NgModule({ declarations: [
-        AppComponent
-    ],
-    bootstrap: [AppComponent], imports: [BrowserModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
-export class AppModule { }
+@NgModule({
+  bootstrap: [AppComponent],
+  imports: [BrowserModule],
+})
+export class AppModule {}
---
+++ src/app/utils/races.ts
@@ -0,0 +1,13 @@
+export const enum Races {
+  HUMAN = 1,
+  ORC = 2,
+  DWARF = 3,
+  NIGHTELF = 4,
+  UNDEAD = 5,
+  TAUREN = 6,
+  GNOME = 7,
+  TROLL = 8,
+  GOBLIN = 9,
+  BLOODELF = 10,
+  DRAENEI = 11,
+}
